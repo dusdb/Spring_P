ffee217e2bb120502527cd228d0337b665eaebf4
Yeonwoo <yunwoo21s@naver.com>
2025-03-23 20:41:00 +0900

    세팅

---
+++ src/test/java/com/Spring1/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring1.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
